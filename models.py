f284e4f3c543cc06db19e9e1e699b5a3f2f2ec60
David Faulkenberry <david.faulkenberry@gmail.com>
2017-11-03 11:55:11 -0400

    Comments

--- models.py
+++ models.py
@@ -15,39 +15,66 @@ class BaseModel(Model):
 
 
 class Rule(BaseModel):
+    # The order in which rules will be applied.
+    # Rules with a higher application order will take precedence,
+    # but only if their pattern applies.
     application_order = IntegerField(null=False, unique=True)
+    # Only active rules are applied.
     active = BooleanField(default=True)
+    # What column of the source data should we match against?
     source_column = CharField(null=False)
+    # What regular expression will we use to match the source data?
     source_pattern = CharField(null=False)
+    # What column of the output data will we use our rule to determine?
     output_column = CharField(null=False)
+    # What string interpolation will we apply to determine that value?
     output_pattern = CharField(null=False)
+    # When was the rule created? For auditing
     created_at = DateTimeField(default=datetime.datetime.now)
 
     def apply(self, line, attributes):
+        # Get the data from the source column as specified in the rule.
         source_data = getattr(line, self.source_column)
+        # Apply the pattern, and pull the named match groups into a dictionary.
         match_dict = re.match(self.source_pattern, source_data).groupdict()
+        # Create a string template based on the rule's template pattern.
         output_template = Template(self.output_pattern)
+        # Apply the dictionary created from the input pattern to the template.
         output_data = output_template.substitute(match_dict)
+        # Set (overwrite) the output column based on the rule.
         attributes[self.output_column] = output_data
         return attributes
 
 
 class SourceLine(BaseModel):
+    # A sample column.
     text = CharField(null=False)
+    # Whether the source line has been processed,
+    # so we don't create multiple output lines per input line.
     processed = BooleanField(default=False)
 
 
 class OutputLine(BaseModel):
+    # A sample column.
     text = CharField(null=False)
+    # A pointer to the source line from which the output line was created.
     source_line = ForeignKeyField(SourceLine)
+    # TODO: A many-to-many reference specifying which rules were applied.
 
 
 if __name__ == '__main__':
     db.connect()
+    # If the INITIALIZE environment variable is given,
+    # recreate the tables from scratch.
     if os.environ.get('INITIALIZE'):
+        # Drop and recreate the tables.
         db.drop_tables([Rule, SourceLine, OutputLine], safe=True)
         db.create_tables([Rule, SourceLine, OutputLine])
+        # Some example data.
         SourceLine.create(text='banana123')
+        # This rule takes any digits after the word 'banana',
+        # and maps that to a more explicit description of the 'banana number'
+        # in the output.
         Rule.create(application_order=1,
                     source_column='text',
                     source_pattern=r'banana(?P<banana_number>\d*)',
@@ -57,8 +84,12 @@ if __name__ == '__main__':
     rules = Rule.select().where(Rule.active == True).order_by(Rule.application_order)
     for source_line in source_lines:
         output_line_attrs = {}
+        # Apply each rule in turn, overwriting as we go (if rules apply).
         for rule in rules:
             output_line_attrs = rule.apply(source_line, output_line_attrs)
+        # Add to the attributes the source line which created the output line.
         output_line_attrs.update(source_line=source_line)
+        # Apply the dictionary of arguments as keyword arguments to `create`.
         OutputLine.create(**output_line_attrs)
+        # Mark the source line as processed.
         source_line.update(processed=True)
